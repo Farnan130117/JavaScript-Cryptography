b3d8cffa1f25905f0bf3aa53e710777b6623de3a
Md. Mokammel Hossen Farnan <Farnan130117@users.noreply.github.com>
2020-07-19 23:50:33 +0600

    Update step_1.js
    update some JS oop cryptography

--- Step_1/step_1.js
+++ Step_1/step_1.js
@@ -62,6 +62,37 @@
 	// Display data from the object:
 	document.getElementById("demo").innerHTML = person.fullName;
 
+//OOP- Defining object 
+let person = { 
+    first_name:'Mukul', 
+    last_name: 'Latiyan', 
+  
+    //method 
+    getFunction : function(){ 
+        return (`The name of the person is  
+          ${person.first_name} ${person.last_name}`) 
+    }, 
+    //object within object 
+    phone_number : { 
+        mobile:'12345', 
+        landline:'6789'
+    } 
+} 
+console.log(person.getFunction());  
+console.log(person.phone_number.landline); 
+
+//OOP - using a constructor 
+function person(first_name,last_name){ 
+   this.first_name = first_name; 
+   this.last_name = last_name; 
+} 
+//creating new instances of person object 
+let person1 = new person('Mukul','Latiyan'); 
+let person2 = new person('Rahul','Avasthi'); 
+  
+console.log(person1.first_name); 
+console.log(`${person2.first_name} ${person2.last_name}`);
+
 //array
 		let selectColour = ['cricket' , 'football']
 		selectColour[1] = 'tennis'
