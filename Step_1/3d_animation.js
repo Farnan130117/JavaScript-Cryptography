b1cae763fb3ff787983012c9b6a683f2b6ba5076
Md. Mokammel Hossen Farnan <Farnan130117@users.noreply.github.com>
2020-09-18 23:47:07 +0600

    Animation with js file added

---
+++ Step_1/3d_animation.js
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Heroic Features - Start Bootstrap Template</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/heroic-features.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<div class="container" style="box-sizing: border-box; width: 1140px; padding-right: 15px; padding-left: 15px; margin-right: auto; margin-left: auto; max-width: 1140px; color: #212529; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'; font-size: 16px;">
+
+<div class="row text-center" style="box-sizing: border-box; display: flex; flex-wrap: wrap; margin-right: -15px; margin-left: -15px; text-align: center !important;">
+<div class="col-lg-3 col-md-6 mb-4" style="box-sizing: border-box; position: relative; width: 285px; padding-right: 15px; padding-left: 15px; flex: 0 0 25%; max-width: 25%; margin-bottom: 1.5rem !important;">
+
+<div class="card h-100" style="box-sizing: border-box; position: relative; display: flex; flex-direction: column; min-width: 0px; overflow-wrap: break-word; background-clip: border-box; border: 1px solid rgba(0, 0, 0, 0.125); border-radius: 0.25rem; height: 430.237px;"><img class="card-img-top" src="https://cdn.pixabay.com/photo/2020/07/25/06/41/mountains-5435903_1280.jpg" alt="" style="box-sizing: border-box; vertical-align: middle; border-style: none; flex-shrink: 0; width: 253px; border-top-left-radius: calc(0.25rem - 1px); border-top-right-radius: calc(0.25rem - 1px);" />
+
+<div class="card-body" style="box-sizing: border-box; flex: 1 1 auto; min-height: 1px; padding: 1.25rem;">
+<h4 class="card-title" style="box-sizing: border-box; margin-top: 0px; margin-bottom: 0.75rem; font-weight: 500; line-height: 1.2; font-size: 1.5rem;">Winter Season</h4>
+<p class="card-text" style="box-sizing: border-box; margin-top: 0px; margin-bottom: 0px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+</div>
+<div class="card-footer" style="box-sizing: border-box; padding: 0.75rem 1.25rem; background-color: rgba(0, 0, 0, 0.03); border-top: 1px solid rgba(0, 0, 0, 0.125); border-radius: 0px 0px calc(0.25rem - 1px) calc(0.25rem - 1px);"><a href="https://startbootstrap.github.io/startbootstrap-heroic-features/" class="btn btn-primary" style="box-sizing: border-box; color: #ffffff; text-decoration-line: none; background-color: #007bff; display: inline-block; vertical-align: middle; user-select: none; border: 1px solid #007bff; padding: 0.375rem 0.75rem; font-size: 1rem; line-height: 1.5; border-radius: 0.25rem; transition: color 0.15s ease-in-out 0s, background-color 0.15s ease-in-out 0s, border-color 0.15s ease-in-out 0s, box-shadow 0.15s ease-in-out 0s; cursor: pointer;">Find Out More!</a></div>
+</div>
+</div>
+<div class="col-lg-3 col-md-6 mb-4" style="box-sizing: border-box; position: relative; width: 285px; padding-right: 15px; padding-left: 15px; flex: 0 0 25%; max-width: 25%; margin-bottom: 1.5rem !important;">
+
+<div class="card h-100" style="box-sizing: border-box; position: relative; display: flex; flex-direction: column; min-width: 0px; overflow-wrap: break-word; background-clip: border-box; border: 1px solid rgba(0, 0, 0, 0.125); border-radius: 0.25rem; height: 430.237px;">
+
+<img class="card-img-top" src="https://cdn.pixabay.com/photo/2020/08/23/03/41/leaves-5509797_1280.jpg" alt="" style="box-sizing: border-box; vertical-align: middle; border-style: none; flex-shrink: 0; width: 253px; border-top-left-radius: calc(0.25rem - 1px); border-top-right-radius: calc(0.25rem - 1px);" />
+<div class="card-body" style="box-sizing: border-box; flex: 1 1 auto; min-height: 1px; padding: 1.25rem;">
+<h4 class="card-title" style="box-sizing: border-box; margin-top: 0px; margin-bottom: 0.75rem; font-weight: 500; line-height: 1.2; font-size: 1.5rem;">Card title</h4>
+<p class="card-text" style="box-sizing: border-box; margin-top: 0px; margin-bottom: 0px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.</p>
+</div>
+<div class="card-footer" style="box-sizing: border-box; padding: 0.75rem 1.25rem; background-color: rgba(0, 0, 0, 0.03); border-top: 1px solid rgba(0, 0, 0, 0.125); border-radius: 0px 0px calc(0.25rem - 1px) calc(0.25rem - 1px);"><a href="https://startbootstrap.github.io/startbootstrap-heroic-features/" class="btn btn-primary" style="box-sizing: border-box; color: #ffffff; text-decoration-line: none; background-color: #007bff; display: inline-block; vertical-align: middle; user-select: none; border: 1px solid #007bff; padding: 0.375rem 0.75rem; font-size: 1rem; line-height: 1.5; border-radius: 0.25rem; transition: color 0.15s ease-in-out 0s, background-color 0.15s ease-in-out 0s, border-color 0.15s ease-in-out 0s, box-shadow 0.15s ease-in-out 0s; cursor: pointer;">Find Out More!</a></div>
+</div>
+</div>
+<div class="col-lg-3 col-md-6 mb-4" style="box-sizing: border-box; position: relative; width: 285px; padding-right: 15px; padding-left: 15px; flex: 0 0 25%; max-width: 25%; margin-bottom: 1.5rem !important;">
+<div class="card h-100" style="box-sizing: border-box; position: relative; display: flex; flex-direction: column; min-width: 0px; overflow-wrap: break-word; background-clip: border-box; border: 1px solid rgba(0, 0, 0, 0.125); border-radius: 0.25rem; height: 430.237px;"><img class="card-img-top" src="https://cdn.pixabay.com/photo/2020/08/29/18/50/coastal-5527726_1280.jpg" alt="" style="box-sizing: border-box; vertical-align: middle; border-style: none; flex-shrink: 0; width: 253px; border-top-left-radius: calc(0.25rem - 1px); border-top-right-radius: calc(0.25rem - 1px);" />
+<div class="card-body" style="box-sizing: border-box; flex: 1 1 auto; min-height: 1px; padding: 1.25rem;">
+<h4 class="card-title" style="box-sizing: border-box; margin-top: 0px; margin-bottom: 0.75rem; font-weight: 500; line-height: 1.2; font-size: 1.5rem;">Card title</h4>
+<p class="card-text" style="box-sizing: border-box; margin-top: 0px; margin-bottom: 0px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+</div>
+<div class="card-footer" style="box-sizing: border-box; padding: 0.75rem 1.25rem; background-color: rgba(0, 0, 0, 0.03); border-top: 1px solid rgba(0, 0, 0, 0.125); border-radius: 0px 0px calc(0.25rem - 1px) calc(0.25rem - 1px);"><a href="https://startbootstrap.github.io/startbootstrap-heroic-features/" class="btn btn-primary" style="box-sizing: border-box; color: #ffffff; text-decoration-line: none; background-color: #007bff; display: inline-block; vertical-align: middle; user-select: none; border: 1px solid #007bff; padding: 0.375rem 0.75rem; font-size: 1rem; line-height: 1.5; border-radius: 0.25rem; transition: color 0.15s ease-in-out 0s, background-color 0.15s ease-in-out 0s, border-color 0.15s ease-in-out 0s, box-shadow 0.15s ease-in-out 0s; cursor: pointer;">Find Out More!</a></div>
+</div>
+</div>
+<div class="col-lg-3 col-md-6 mb-4" style="box-sizing: border-box; position: relative; width: 285px; padding-right: 15px; padding-left: 15px; flex: 0 0 25%; max-width: 25%; margin-bottom: 1.5rem !important;">
+<div class="card h-100" style="box-sizing: border-box; position: relative; display: flex; flex-direction: column; min-width: 0px; overflow-wrap: break-word; background-clip: border-box; border: 1px solid rgba(0, 0, 0, 0.125); border-radius: 0.25rem; height: 430.237px;"><img class="card-img-top" src="https://cdn.pixabay.com/photo/2020/08/14/23/33/child-5489290_1280.jpg" alt="" style="box-sizing: border-box; vertical-align: middle; border-style: none; flex-shrink: 0; width: 253px; border-top-left-radius: calc(0.25rem - 1px); border-top-right-radius: calc(0.25rem - 1px);" />
+<div class="card-body" style="box-sizing: border-box; flex: 1 1 auto; min-height: 1px; padding: 1.25rem;">
+<h4 class="card-title" style="box-sizing: border-box; margin-top: 0px; margin-bottom: 0.75rem; font-weight: 500; line-height: 1.2; font-size: 1.5rem;">Card title</h4>
+<p class="card-text" style="box-sizing: border-box; margin-top: 0px; margin-bottom: 0px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.</p>
+</div>
+<div class="card-footer" style="box-sizing: border-box; padding: 0.75rem 1.25rem; background-color: rgba(0, 0, 0, 0.03); border-top: 1px solid rgba(0, 0, 0, 0.125); border-radius: 0px 0px calc(0.25rem - 1px) calc(0.25rem - 1px);"><a href="https://startbootstrap.github.io/startbootstrap-heroic-features/" class="btn btn-primary" style="box-sizing: border-box; color: #ffffff; text-decoration-line: none; background-color: #007bff; display: inline-block; vertical-align: middle; user-select: none; border: 1px solid #007bff; padding: 0.375rem 0.75rem; font-size: 1rem; line-height: 1.5; border-radius: 0.25rem; transition: color 0.15s ease-in-out 0s, background-color 0.15s ease-in-out 0s, border-color 0.15s ease-in-out 0s, box-shadow 0.15s ease-in-out 0s; cursor: pointer;">Find Out More!</a></div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="container" style="box-sizing: border-box; width: 1140px; padding-right: 15px; padding-left: 15px; margin-right: auto; margin-left: auto; max-width: 1140px; color: #212529; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'; font-size: 16px;">
+
+<div class="row text-center" style="box-sizing: border-box; display: flex; flex-wrap: wrap; margin-right: -15px; margin-left: -15px; text-align: center !important;">
+<div class="col-lg-3 col-md-6 mb-4" style="box-sizing: border-box; position: relative; width: 285px; padding-right: 15px; padding-left: 15px; flex: 0 0 25%; max-width: 25%; margin-bottom: 1.5rem !important;">
+
+<div class="card h-100" style="box-sizing: border-box; position: relative; display: flex; flex-direction: column; min-width: 0px; overflow-wrap: break-word; background-clip: border-box; border: 1px solid rgba(0, 0, 0, 0.125); border-radius: 0.25rem; height: 430.237px;"><img class="card-img-top" src="https://cdn.pixabay.com/photo/2020/07/25/06/41/mountains-5435903_1280.jpg" alt="" style="box-sizing: border-box; vertical-align: middle; border-style: none; flex-shrink: 0; width: 253px; border-top-left-radius: calc(0.25rem - 1px); border-top-right-radius: calc(0.25rem - 1px);" />
+
+<div class="card-body" style="box-sizing: border-box; flex: 1 1 auto; min-height: 1px; padding: 1.25rem;">
+<h4 class="card-title" style="box-sizing: border-box; margin-top: 0px; margin-bottom: 0.75rem; font-weight: 500; line-height: 1.2; font-size: 1.5rem;">Winter Season</h4>
+<p class="card-text" style="box-sizing: border-box; margin-top: 0px; margin-bottom: 0px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+</div>
+<div class="card-footer" style="box-sizing: border-box; padding: 0.75rem 1.25rem; background-color: rgba(0, 0, 0, 0.03); border-top: 1px solid rgba(0, 0, 0, 0.125); border-radius: 0px 0px calc(0.25rem - 1px) calc(0.25rem - 1px);"><a href="https://startbootstrap.github.io/startbootstrap-heroic-features/" class="btn btn-primary" style="box-sizing: border-box; color: #ffffff; text-decoration-line: none; background-color: #007bff; display: inline-block; vertical-align: middle; user-select: none; border: 1px solid #007bff; padding: 0.375rem 0.75rem; font-size: 1rem; line-height: 1.5; border-radius: 0.25rem; transition: color 0.15s ease-in-out 0s, background-color 0.15s ease-in-out 0s, border-color 0.15s ease-in-out 0s, box-shadow 0.15s ease-in-out 0s; cursor: pointer;">Find Out More!</a></div>
+</div>
+</div>
+<div class="col-lg-3 col-md-6 mb-4" style="box-sizing: border-box; position: relative; width: 285px; padding-right: 15px; padding-left: 15px; flex: 0 0 25%; max-width: 25%; margin-bottom: 1.5rem !important;">
+
+<div class="card h-100" style="box-sizing: border-box; position: relative; display: flex; flex-direction: column; min-width: 0px; overflow-wrap: break-word; background-clip: border-box; border: 1px solid rgba(0, 0, 0, 0.125); border-radius: 0.25rem; height: 430.237px;">
+
+<img class="card-img-top" src="https://cdn.pixabay.com/photo/2020/08/23/03/41/leaves-5509797_1280.jpg" alt="" style="box-sizing: border-box; vertical-align: middle; border-style: none; flex-shrink: 0; width: 253px; border-top-left-radius: calc(0.25rem - 1px); border-top-right-radius: calc(0.25rem - 1px);" />
+<div class="card-body" style="box-sizing: border-box; flex: 1 1 auto; min-height: 1px; padding: 1.25rem;">
+<h4 class="card-title" style="box-sizing: border-box; margin-top: 0px; margin-bottom: 0.75rem; font-weight: 500; line-height: 1.2; font-size: 1.5rem;">Card title</h4>
+<p class="card-text" style="box-sizing: border-box; margin-top: 0px; margin-bottom: 0px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.</p>
+</div>
+<div class="card-footer" style="box-sizing: border-box; padding: 0.75rem 1.25rem; background-color: rgba(0, 0, 0, 0.03); border-top: 1px solid rgba(0, 0, 0, 0.125); border-radius: 0px 0px calc(0.25rem - 1px) calc(0.25rem - 1px);"><a href="https://startbootstrap.github.io/startbootstrap-heroic-features/" class="btn btn-primary" style="box-sizing: border-box; color: #ffffff; text-decoration-line: none; background-color: #007bff; display: inline-block; vertical-align: middle; user-select: none; border: 1px solid #007bff; padding: 0.375rem 0.75rem; font-size: 1rem; line-height: 1.5; border-radius: 0.25rem; transition: color 0.15s ease-in-out 0s, background-color 0.15s ease-in-out 0s, border-color 0.15s ease-in-out 0s, box-shadow 0.15s ease-in-out 0s; cursor: pointer;">Find Out More!</a></div>
+</div>
+</div>
+<div class="col-lg-3 col-md-6 mb-4" style="box-sizing: border-box; position: relative; width: 285px; padding-right: 15px; padding-left: 15px; flex: 0 0 25%; max-width: 25%; margin-bottom: 1.5rem !important;">
+<div class="card h-100" style="box-sizing: border-box; position: relative; display: flex; flex-direction: column; min-width: 0px; overflow-wrap: break-word; background-clip: border-box; border: 1px solid rgba(0, 0, 0, 0.125); border-radius: 0.25rem; height: 430.237px;"><img class="card-img-top" src="https://cdn.pixabay.com/photo/2020/08/29/18/50/coastal-5527726_1280.jpg" alt="" style="box-sizing: border-box; vertical-align: middle; border-style: none; flex-shrink: 0; width: 253px; border-top-left-radius: calc(0.25rem - 1px); border-top-right-radius: calc(0.25rem - 1px);" />
+<div class="card-body" style="box-sizing: border-box; flex: 1 1 auto; min-height: 1px; padding: 1.25rem;">
+<h4 class="card-title" style="box-sizing: border-box; margin-top: 0px; margin-bottom: 0.75rem; font-weight: 500; line-height: 1.2; font-size: 1.5rem;">Card title</h4>
+<p class="card-text" style="box-sizing: border-box; margin-top: 0px; margin-bottom: 0px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+</div>
+<div class="card-footer" style="box-sizing: border-box; padding: 0.75rem 1.25rem; background-color: rgba(0, 0, 0, 0.03); border-top: 1px solid rgba(0, 0, 0, 0.125); border-radius: 0px 0px calc(0.25rem - 1px) calc(0.25rem - 1px);"><a href="https://startbootstrap.github.io/startbootstrap-heroic-features/" class="btn btn-primary" style="box-sizing: border-box; color: #ffffff; text-decoration-line: none; background-color: #007bff; display: inline-block; vertical-align: middle; user-select: none; border: 1px solid #007bff; padding: 0.375rem 0.75rem; font-size: 1rem; line-height: 1.5; border-radius: 0.25rem; transition: color 0.15s ease-in-out 0s, background-color 0.15s ease-in-out 0s, border-color 0.15s ease-in-out 0s, box-shadow 0.15s ease-in-out 0s; cursor: pointer;">Find Out More!</a></div>
+</div>
+</div>
+<div class="col-lg-3 col-md-6 mb-4" style="box-sizing: border-box; position: relative; width: 285px; padding-right: 15px; padding-left: 15px; flex: 0 0 25%; max-width: 25%; margin-bottom: 1.5rem !important;">
+<div class="card h-100" style="box-sizing: border-box; position: relative; display: flex; flex-direction: column; min-width: 0px; overflow-wrap: break-word; background-clip: border-box; border: 1px solid rgba(0, 0, 0, 0.125); border-radius: 0.25rem; height: 430.237px;"><img class="card-img-top" src="https://cdn.pixabay.com/photo/2020/08/14/23/33/child-5489290_1280.jpg" alt="" style="box-sizing: border-box; vertical-align: middle; border-style: none; flex-shrink: 0; width: 253px; border-top-left-radius: calc(0.25rem - 1px); border-top-right-radius: calc(0.25rem - 1px);" />
+<div class="card-body" style="box-sizing: border-box; flex: 1 1 auto; min-height: 1px; padding: 1.25rem;">
+<h4 class="card-title" style="box-sizing: border-box; margin-top: 0px; margin-bottom: 0.75rem; font-weight: 500; line-height: 1.2; font-size: 1.5rem;">Card title</h4>
+<p class="card-text" style="box-sizing: border-box; margin-top: 0px; margin-bottom: 0px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.</p>
+</div>
+<div class="card-footer" style="box-sizing: border-box; padding: 0.75rem 1.25rem; background-color: rgba(0, 0, 0, 0.03); border-top: 1px solid rgba(0, 0, 0, 0.125); border-radius: 0px 0px calc(0.25rem - 1px) calc(0.25rem - 1px);"><a href="https://startbootstrap.github.io/startbootstrap-heroic-features/" class="btn btn-primary" style="box-sizing: border-box; color: #ffffff; text-decoration-line: none; background-color: #007bff; display: inline-block; vertical-align: middle; user-select: none; border: 1px solid #007bff; padding: 0.375rem 0.75rem; font-size: 1rem; line-height: 1.5; border-radius: 0.25rem; transition: color 0.15s ease-in-out 0s, background-color 0.15s ease-in-out 0s, border-color 0.15s ease-in-out 0s, box-shadow 0.15s ease-in-out 0s; cursor: pointer;">Find Out More!</a></div>
+</div>
+</div>
+</div>
+</div>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
